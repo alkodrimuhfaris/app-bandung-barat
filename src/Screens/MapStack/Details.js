80712b7028ef84dbcd39ec0b22960bb5ca64eb0a
alkodrimuhfaris <farisalkodri@gmail.com>
2021-04-26 10:34:54 +0700

    Edit: details on map

--- src/Screens/MapStack/Details.js
+++ src/Screens/MapStack/Details.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, Dimensions, StyleSheet} from 'react-native';
+import detailArray from '../../Helpers/detailArray';
 import detailArr from '../../Helpers/detailArray';
+
+const {width, height} = Dimensions.get('screen');
+const paddingCoef = 0.97;
 
 export default function Details({navigation, route}) {
   const {id} = route.params;
-
+  const [details, setDetails] = React.useState({});
   React.useEffect(() => {
     if (Number(id)) {
-      const title = detailArr[1].name;
+      const index = Number(id);
+      setDetails(detailArray[index]);
+      const title = detailArr[index].name;
       navigation.setOptions({
         title,
       });
     }
   }, [id]);
-  return (
+  return Object.entries(details).length ? (
     <View style={styles.parent}>
-      <Text>{id}</Text>
+      <View style={styles.carouselWrapper}>
+        <Text>This is carouselWrapper</Text>
+      </View>
+      <View style={styles.detailTextWrapper}>
+        <Text style={styles.title}>{details.name}</Text>
+      </View>
+    </View>
+  ) : (
+    <View style={styles.parent}>
+      <Text>Nothing here</Text>
     </View>
   );
 }
@@ -25,5 +40,18 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     backgroundColor: 'white',
+  },
+  carouselWrapper: {
+    width: '100%',
+    height: height * 0.35,
+    backgroundColor: 'red',
+  },
+  detailTextWrapper: {
+    width: width * paddingCoef,
+    padding: (width * (1 - paddingCoef)) / 2,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 10,
   },
 });
